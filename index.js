552cd3d43832f981c6f92bef5d38757071663ca3
SANAGOdev <55107023+SANAGOdev@users.noreply.github.com>
2023-03-25 16:19:25 +0100

    Update

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const fs = require('fs');
 const validator = require('validator');
 const axios = require('axios');
+const https = require('https');
 const apiKey = 'Your API key'; // https://hunter.io/ se faire un compte pour recup l'API key
 
-const fileContents = fs.readFileSync('emails.txt', 'utf-8'); // Remplace "email.txt" par le nom de ton ficher avec les emails.
+const fileContents = fs.readFileSync('emails.txt', 'utf-8'); // Remplacez "emails.txt" par le nom de votre ficher avec les emails.
 const emailArray = fileContents.split('\n');
 
 const validEmails = [];
 const invalidEmails = [];
 
 function getRandomProxy() {
-  const proxies = fs.readFileSync('proxy.txt', 'utf-8').trim().split('\n'); // Ici le noms du fichiers qui contiens les proxies.
+  const proxies = fs.readFileSync('proxy.txt', 'utf-8').trim().split('\n'); // Ici le nom du fichier qui contiens les proxy.
   return proxies[Math.floor(Math.random() * proxies.length)];
 }
 
 function verifyEmail(email) {
   const proxy = getRandomProxy();
   const instance = axios.create({
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+        secureProtocol: 'TLSv1_2_method' //On force l'utilisation d'une version spécifique de TLS
+    }),
     proxy: {
       host: proxy.split(':')[0],
       port: proxy.split(':')[1],
@@ -24,7 +29,7 @@ function verifyEmail(email) {
   });
   
   let url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`;
-  axios.get(url)
+  instance.get(url)
     .then((response) => {
         const data = response.data;
         if (data.data.result == "deliverable") {
